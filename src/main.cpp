876f7ce82331bd17ba07744cf5912e5c295d2dad
Dave Cowden <dave.cowden@gmail.com>
2019-02-02 22:44:44 -0500

    working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_MCP23017.h"
 
 const byte LED_PIN_OUT = 3;
 const byte LED_PIN_IN = 2;
 
+Adafruit_MCP23017 mcp;
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_PIN_OUT, OUTPUT);
-  pinMode(LED_PIN_IN, INPUT);
+
+
+  mcp.begin();      // use default address 0
+
+  mcp.pinMode(0, INPUT);
+  mcp.pullUp(0, HIGH);  // turn on a 100K pullup internally
+
+  pinMode(13, OUTPUT);  // use the p13 LED as debugging
+
+
 }
 
 void loop() {
-  digitalWrite(LED_PIN_OUT, !digitalRead(LED_PIN_IN));   // turn the LED on
+  digitalWrite(13, mcp.digitalRead(0));
 }
