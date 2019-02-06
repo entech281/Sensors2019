2d1689b2f04bb19fcf8ed23cb017f7880efae695
Your Name <you@example.com>
2019-02-05 19:35:51 -0500

    got logic to find distance and started on i2c

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,62 @@
 #include <Arduino.h>
+#include "Adafruit_MCP23017.h"
+#include <Wire.h>
 
-const byte LED_PIN_OUT = 3;
-const byte LED_PIN_IN = 2;
+#define DISTANCE_BETWEEN_SENSORS 2
+#define SENSOR_AREA_WIDTH        4
+#define RIO_I2C_PORT             8
 
-void setup() {
+/*const byte LED_PIN_OUT = ;
+const byte LED_PIN_IN = ;
+*/
+Adafruit_MCP23017 port_expander;
+
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(LED_PIN_OUT, OUTPUT);
-  pinMode(LED_PIN_IN, INPUT);
+  Serial.begin(9600);
+  while (!Serial)
+  {
+  }
+
+  Serial.println("Serial set up");
+
+  port_expander.begin(0);
+  Wire.begin();
+  Serial.println("IO Expander Initialized");
+
+  port_expander.pinMode(0, INPUT);
+  port_expander.pinMode(1, INPUT);
+  port_expander.pinMode(2, INPUT);
+  port_expander.pinMode(3, INPUT);
+
+  pinMode(2, OUTPUT);
+  Serial.println("IO Expander Pins Set");
+  //port_expander.pullUp(0, HIGH);
 }
 
-void loop() {
-  digitalWrite(LED_PIN_OUT, !digitalRead(LED_PIN_IN));   // turn the LED on
+void loop()
+{
+  uint16_t bits = port_expander.readGPIOAB();
+  uint16_t indexRight = 0 ;
+  for(indexRight = 0; indexRight <= 16; ++indexRight){
+     if(!( bits << indexRight )){
+       break;
+     }
+  }
+  indexRight --;
+  int indexLeft;
+  for(indexLeft = 0; indexLeft <= 16; ++indexLeft){
+     if(!( bits >> indexLeft )){
+       break;
+     }
+  }
+  indexLeft ++;
+  indexLeft = indexLeft - 16;
+
+  int offset_from_robot = ( indexRight + indexLeft ) / 2 * DISTANCE_BETWEEN_SENSORS - SENSOR_AREA_WIDTH / 2;
+  Wire.beginTransmission(RIO_I2C_PORT);
+  Wire.write(offset_from_robot);
+  Wire.endTransmission();
+
 }
